56a15a20ffad12de059dee322e23fbae7908fe94
elli-ma <elli9225@gmail.com>
2023-06-12 22:14:20 +0800

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 function Skills() {
     const { t } = useTranslation();
     return (
-        <section  className="block">
+        <section className="block">
             <div className="block_col3">
                 <h2 className="block__title" >{t("skills.title")}</h2>
                 {/* <p className="block__text">Junior Frontend Saint Petersburg</p> */}
@@ -16,12 +16,17 @@ function Skills() {
             <div className="col4">
                 <div className="col4__item">
                     <h3 className="col4__title">{t("skills.item1")}</h3>
-                    <p className="col4__text">HTML, CSS, SASS, Bootstrap, React, Javascript, Node.js, Typescript.</p>
+                    <p className="col4__text">HTML, SASS, Bootstrap, React, Javascript, Typescript.</p>
+                </div>
+                <div className="col4__item">
+                    <h3 className="col4__title">Backend Development</h3>
+                    <p id="education" className="col4__text">Nodejs, Express, C#, Docker.</p>
                 </div>
                 <div className="col4__item">
                     <h3 className="col4__title">{t("skills.item2")}</h3>
-                    <p id ="education" className="col4__text">Photoshop, Illustrator, CorelDRAW.</p>
-                </div>
+                    <p id="education" className="col4__text">Photoshop, Illustrator, Figma, CorelDRAW.</p>
+                </div >
+
             </div>
         </section>
     );
